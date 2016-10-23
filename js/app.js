4e40331f1442cdc23eef8980ec9b7e1b7d7eb755
Vitaly Govor <vitaly.govor@icloud.com>
2016-10-23 14:49:57 +0300

    Full prototype

--- js/app.js
+++ js/app.js
@@ -1,6 +1,96 @@
 $(document).ready(function () {
 
-    var Memory = {};
+    var Results = 0;
+
+    var Core = {
+        fire:{
+            name: "fire",
+            color: "green",
+            link: {
+                metal:"gold",
+                clay: "brick"
+            }
+        },
+        gold: {
+            name: "gold",
+            color: "yellow",
+            link: {
+                abacus:"bank"
+            }
+        },
+        brick: {
+            name: "brick",
+            color: "brown",
+            link: {
+                instruments:"building"
+            }
+        },
+        instruments: {
+            name: "instruments",
+            color: "blue",
+            link: {
+                brick:"building"
+            }
+        },
+        building: {
+            name: "building",
+            color: "red",
+        },
+        abacus: {
+            name: "abacus",
+            color: "brown",
+            link: {
+                gold: "bank"
+            }
+        },
+        bank: {
+            name: "bank",
+            color: "red",
+        },
+        metal: {
+            name: "metal",
+            color: "black",
+            link: {
+                fire:"gold"
+            }
+        }
+    }
+
+    //  Scan all blocks in .battleblock and change styles
+    function scanBlocks() {
+        // console.log($(".battleplace .block .block").length);
+
+        $(".battleplace .block .block").each(function(index) {
+            $(this).css({
+                "background":Core[$(this).attr("element")].color,
+                "margin": 0
+            })
+        });
+
+    }
+
+    function getNewRandomElement() {
+
+        // Generate random number
+        var Random = Math.floor((Math.random() * Object.keys(Core).length) + 1);
+
+        // Keys of Core
+        var keysOfCore = Object.keys(Core);
+
+        // Sort all of Core's values
+        if(Core[keysOfCore[Random-1]].hasOwnProperty('link')){
+            return Core[keysOfCore[Random-1]];
+        } else {
+            return getNewRandomElement();
+        }
+
+    }
+
+    function createNewElement(Obj) {
+        $(".dinamicBlock").html("<div class='block' element='"+Obj.name+"' style='background: "+Obj.color+"'>"+Obj.name+"</div>");
+    }
+
+    createNewElement(getNewRandomElement());
 
     function doDraggable() {
         $(".dinamicBlock .block").draggable({
@@ -8,130 +98,106 @@ $(document).ready(function () {
         });
     }
 
+    function checkAccessBlocks(block1, block2){
+
+        var element = block2[0].attributes.element;
+        var elementName = element.name;
+        var elementValue = element.value;
+
+        if($(block1).find(".block").length == 0) {
+
+            block1.html("<div class='block' "+elementName+"='"+elementValue+"'>"+block2.text()+"</div>");
+
+            plusPoints();
+
+        } else {
+
+            var mainElement = $(block1).find(".block").attr("element");
+            var mainKeys = Core[mainElement].link;
+
+            if (mainKeys != undefined) {
+
+                if (mainKeys.hasOwnProperty(elementValue)) {
+                    block1.html("<div class='block' "+elementName+"='"+Core[mainElement].link[elementValue]+"'>"+Core[mainElement].link[elementValue]+"</div>");
+
+                    plusPoints();
+                } else {
+                    minusPoints();
+                }
+            }
+        }
+
+        updateResults();
+    }
+
     doDraggable();
-    checkColor();
 
-    function checkColor() {
-        var el = $(".dinamicBlock .block");
-        // alert(el.attr('level'));
-
-        if(el.attr('level') == 0) {
-            el.css({
-                "background": "#666"
-            });
-        }
-
-        if(el.attr('level') == 1) {
-            el.css({
-                "background": "#709C34"
-            });
-        }
-
-        if(el.attr('level') == 2) {
-            el.css({
-                "background": "#FFD300"
-            });
-        }
-
-        if(el.attr('level') == 3) {
-            el.css({
-                "background": "#FF4900"
-            });
-        }
+    function plusPoints() {
+        Results += 100;
     }
 
-    function RandomElement() {
-        var Ran1 = Math.floor((Math.random() * 3) + 1);
-        var Ran2 = Math.floor((Math.random() * 3) + 1);
-
-        $(".dinamicBlock").html("<div class='block' level='"+Ran1+"' type='"+Ran2+"'>"+Ran2+"</div>");
+    function minusPoints() {
+        Results -= 100;
     }
+
+    function updateResults(){
+        $(".points").html(Results);
+    }
+
+    function getResults() {
+
+    }
+
 
     $(".battleplace .block").droppable({
         accept: ".block",
         drop: function (event, ui) {
 
-            var attrLevel = $(this).attr("level");
-            var attrDropLevel = ui.draggable.attr("level");
-
-            var attrType = $(this).attr("type");
-            var attrDropType = ui.draggable.attr("type");
-
-            if($(this).is("[level]")){
-
-                if(attrDropType != attrType){
-                    alert("Error");
-                    $(this).remove();
-                    ui.draggable.remove();
-                } else {
-                    alert(attrDropLevel+" and"+attrLevel);
-                    if(attrLevel == attrDropLevel) {
-                        alert("Error");
-                        $(this).remove();
-                        ui.draggable.remove();
-                    } else {
-
-                        if((attrDropLevel - attrLevel) == 1) {
-                            alert("Success!");
-                            $(this).html(ui.draggable.attr('type'));
-                            $(this).attr("level", ui.draggable.attr('level'));
-
-                            Memory[""+$('.block').index(this)+""] = {
-                                level: ui.draggable.attr('level'),
-                                type: ui.draggable.attr('type')
-                            };
-
-                        } else {
-                            alert("Error");
-                            $(this).remove();
-                            ui.draggable.remove();
-                        }
-                    }
-                }
-
-            } else {
-                $(this).append(ui.draggable.attr('type'));
-                $(this).attr("level", ui.draggable.attr('level'));
-                $(this).attr("type", ui.draggable.attr('type'));
-
-                Memory[""+$('.block').index(this)+""] = {
-                    level: ui.draggable.attr('level'),
-                    type: ui.draggable.attr('type')
-                };
-            }
-
-
-
+            checkAccessBlocks($(this), ui.draggable);
             ui.draggable.hide();
-
-            //Buffer
-            console.log(Memory);
-
-            if(ui.draggable.attr('level') == 1) {
-                $(this).css({
-                    "background": "#709C34"
-                });
-            }
-
-            if(ui.draggable.attr('level') == 2) {
-                $(this).css({
-                    "background": "#FFD300"
-                });
-            }
-
-            if(ui.draggable.attr('level') == 3) {
-                $(this).css({
-                    "background": "#FF4900"
-                });
-            }
-
-            RandomElement();
-            checkColor();
+            scanBlocks();
+            createNewElement(getNewRandomElement());
             doDraggable();
 
         }
     });
 
+    $(".time").hide('');
+    $(".points").hide('');
+
+    $(".start").click(function () {
+        $(".time").show('');
+        $(".points").show('');
+        $(".points").html('0');
+        $(".battleplace .block").html('');
+
+        $(".start").hide();
+
+        var time = 60;
+
+        setInterval(function() {
+
+            $(".time").html(time);
+            console.log(time);
+
+            if(time == 0) {
+                var r = confirm("Your score: "+Results+"! Do you want to play again?");
+                if (r == true) {
+                    $(".time").html('');
+                    $(".points").html('');
+                    $(".start").hide();
+                    $(".battleplace .block").html('');
+                    time = 60;
+                    Results = 0;
+                } else {
+                    location.reload();
+                }
+            } else {
+                time--;
+            }
+        }, 1000);
+
+    });
 
 
 
