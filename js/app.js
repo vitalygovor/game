233210eec755f8b333a4ad6bd86033b296c20eb8
Vitaly Govor <vitaly.govor@icloud.com>
2016-10-31 00:13:12 +0300

    Added new icons and added comments

--- js/app.js
+++ js/app.js
@@ -1,16 +1,17 @@
 $(document).ready(function () {
 
+    //  CACHE
     var Results = 0;
-
     var AwardsMemory = {
         bank: 0,
         building: 0
     };
 
+    //  GENERAL SETTINGS
     var Core = {
         fire:{
             name: "fire",
-            color: "orange",
+            color: "white",
             link: {
                 metal:"gold",
                 clay: "brick"
@@ -18,39 +19,46 @@ $(document).ready(function () {
         },
         gold: {
             name: "gold",
-            color: "#B96E00",
+            color: "white",
             link: {
                 abacus:"bank"
             }
         },
         brick: {
             name: "brick",
-            color: "brown",
+            color: "white",
             link: {
                 instruments:"building"
             }
         },
+        clay: {
+            name: "clay",
+            color: "white",
+            link: {
+                fire:"brick"
+            }
+        },
         instruments: {
             name: "instruments",
-            color: "blue",
+            color: "white",
             link: {
                 brick:"building"
             }
         },
         building: {
             name: "building",
-            color: "red"
+            color: "white"
         },
         abacus: {
             name: "abacus",
-            color: "brown",
+            color: "white",
             link: {
                 gold: "bank"
             }
         },
         bank: {
             name: "bank",
-            color: "red"
+            color: "white"
         },
         metal: {
             name: "metal",
@@ -61,7 +69,7 @@ $(document).ready(function () {
         }
     }
 
-    //  Scan all blocks in .battleblock and change styles
+    //  STYLE SCANNER
     function scanBlocks() {
         // console.log($(".battleplace .block .block").length);
 
@@ -70,7 +78,7 @@ $(document).ready(function () {
             $(this).css({
                 "background-color":Core[$(this).attr("element")].color,
                 "background-image":"url('img/"+Core[$(this).attr("element")].name+".png')",
-                "background-size":"50%",
+                "background-size":"80%",
                 "background-repeat":"no-repeat",
                 "background-position":"center",
                 "margin": 0
@@ -79,197 +87,7 @@ $(document).ready(function () {
 
     }
 
-    function getNewRandomElement() {
-
-        // Generate random number
-        var Random = Math.floor((Math.random() * Object.keys(Core).length) + 1);
-
-        // Keys of Core
-        var keysOfCore = Object.keys(Core);
-
-        // Sort all of Core's values
-        if(Core[keysOfCore[Random-1]].hasOwnProperty('link')){
-            return Core[keysOfCore[Random-1]];
-        } else {
-            return getNewRandomElement();
-        }
-
-    }
-
-    function createNewElement(Obj) {
-
-        var newElement = $("<div />", {
-            class: "block",
-            element: Obj.name,
-            css: {
-                "background-color":Obj.color,
-                "background-image":"url('img/"+Obj.name+".png')",
-                "background-size":"70%",
-                "background-repeat":"no-repeat",
-                "background-position":"center",
-            }
-        });
-
-        $(".dinamicBlock").html(newElement);
-
-    }
-
-    createNewElement(getNewRandomElement());
-
-
-
-    function checkAccessBlocks(block1, block2){
-
-        var element = block2[0].attributes.element;
-        var elementName = element.name;
-        var elementValue = element.value;
-
-        if($(block1).find(".block").length == 0) {
-
-            block1.html("<div class='block' "+elementName+"='"+elementValue+"' level='midddle'></div>");
-            plusPoints();
-            createNewElement(getNewRandomElement());
-
-        } else {
-
-            var mainElement = $(block1).find(".block").attr("element");
-            var mainKeys = Core[mainElement].link;
-
-            if (mainKeys != undefined) {
-
-                if (mainKeys.hasOwnProperty(elementValue)) {
-                    block1.html("<div class='block' "+elementName+"='"+Core[mainElement].link[elementValue]+"' level='midddle'></div>");
-
-                    // Combo.push(Core[mainElement].link[elementValue]);
-                    AwardsMemory[Core[mainElement].link[elementValue]]++;
-
-                    plusPoints();
-                    createNewElement(getNewRandomElement());
-
-                } else {
-                    minusPoints();
-
-                    $(".dinamicBlock").html("<div class='block' element='"+elementValue+"'></div>");
-                    $(".dinamicBlock .block").css({
-                        "background-color": Core[elementValue].color,
-                        "background-image":"url('img/"+Core[elementValue].name+".png')",
-                        "background-size":"70%",
-                        "background-repeat":"no-repeat",
-                        "background-position":"center",
-                    });
-                }
-            } else {
-                $(".dinamicBlock").html("<div class='block' element='"+elementValue+"'></div>");
-                $(".dinamicBlock .block").css({
-                    "background-color": Core[elementValue].color,
-                    "background-image":"url('img/"+Core[elementValue].name+".png')",
-                    "background-size":"70%",
-                    "background-repeat":"no-repeat",
-                    "background-position":"center",
-                });
-            }
-        }
-
-        updateResults();
-    }
-
-
-    function plusPoints() {
-        Results += 100;
-    }
-
-    function minusPoints() {
-        Results -= 100;
-    }
-
-    function updateResults(){
-        $(".points").html(Results);
-    }
-
-    function checkAwards(){
-
-        $(".awards").html('');
-
-        for(var name in AwardsMemory) {
-            // propertyName is what you want
-            // you can get the value like this: myObject[propertyName]
-
-            if((AwardsMemory[name] < 3)&&(AwardsMemory[name] > 0)) {
-                $(".awards").append("<div class='award'><h4>"+name+" = "+AwardsMemory[name]+"!</h4></div>");
-            }
-
-            if(AwardsMemory[name] == 3) {
-                $(".awards").append("<div class='award'><h4>"+name+" > 3! WOOOOW!</h4></div>");
-            }
-
-            if((AwardsMemory[name] < 6)&&(AwardsMemory[name] > 3)) {
-                $(".awards").append("<div class='award'><h4>"+name+" = "+AwardsMemory[name]+"!</h4></div>");
-            }
-
-            if(AwardsMemory[name] == 6) {
-                $(".awards").append("<div class='award'><h4>"+name+" > 6! AWESOME!</h4></div>");
-            }
-
-            if((AwardsMemory[name] < 9)&&(AwardsMemory[name] > 6)) {
-                $(".awards").append("<div class='award'><h4>"+name+" = "+AwardsMemory[name]+"!</h4></div>");
-            }
-
-        }
-    }
-
-    function clearAwards(){
-
-        $(".awards").html('');
-
-        for(var name in AwardsMemory) {
-            // propertyName is what you want
-            // you can get the value like this: myObject[propertyName]
-
-            AwardsMemory[name] = 0;
-
-        }
-    }
-
-
-    $(".time").hide('');
-    $(".points").hide('');
-    $(".start").click(function () {
-        $(".time").show('');
-        $(".points").show('');
-        $(".points").html('0');
-        $(".battleplace .block").html('');
-
-        $(".start").hide();
-
-        var time = 60;
-
-        setInterval(function() {
-
-            $(".time").html(time);
-            console.log(time);
-
-            if(time == 0) {
-                var r = confirm("Your score: "+Results+"! Do you want to play again?");
-                if (r == true) {
-                    $(".time").html('');
-                    $(".points").html('');
-                    $(".start").hide();
-                    $(".battleplace .block").html('');
-                    time = 60;
-                    Results = 0;
-                    clearAwards();
-                } else {
-                    location.reload();
-                }
-            } else {
-                time--;
-            }
-        }, 1000);
-
-    });
-
-    // --------------------------------
-
+    //  MAIN CLASS
     var App = function App(block) {
         this.dinamicArea = $(".dinamicBlock");
         this.battleArea = $(".battleplace");
@@ -287,6 +105,7 @@ $(document).ready(function () {
         this.accessBlock = this.propertiesOfBlock.link;
     };
 
+    //  MAIN CONTROLLERS
     $( "body" ).on("mouseover", '.block' ,function() {
 
         //  Create a new class
@@ -301,7 +120,6 @@ $(document).ready(function () {
         doDroppable(block);
 
     });
-
     function doDraggable(block) {
 
         $(block.selectBlock).draggable({
@@ -320,7 +138,26 @@ $(document).ready(function () {
         });
 
     }
+    function checkAllSectors(block) {
 
+        var DroppableSectors = [];
+
+        $(".battleplace .sector").each(function(index){
+
+            var mainKeys = Core[block.propertiesOfBlock.name].link;
+            if($(this).html().length > 0){
+                var blockIntoSector = new App($(this).find('.block'));
+                if(mainKeys.hasOwnProperty(blockIntoSector.propertiesOfBlock.name)){
+                    DroppableSectors.push(blockIntoSector.parentBlock);
+                }
+            } else {
+                DroppableSectors.push($(this));
+            }
+        });
+
+        return DroppableSectors;
+
+    }
     function doDroppable(block) {
 
         $(checkAllSectors(block)).droppable({
@@ -362,34 +199,13 @@ $(document).ready(function () {
 
                 generateNewBlock(getNewRandomElement());
                 scanBlocks();
+                plusPoints();
+                updateResults();
                 checkAwards();
                 // createNewElement(getNewRandomElement());
             }
         });
     }
-
-    function checkAllSectors(block) {
-
-        var DroppableSectors = [];
-
-        $(".battleplace .sector").each(function(index){
-
-            var mainKeys = Core[block.propertiesOfBlock.name].link;
-            if($(this).html().length > 0){
-                var blockIntoSector = new App($(this).find('.block'));
-                if(mainKeys.hasOwnProperty(blockIntoSector.propertiesOfBlock.name)){
-
-                    DroppableSectors.push(blockIntoSector.parentBlock);
-                }
-            } else {
-                DroppableSectors.push($(this));
-            }
-        });
-
-        return DroppableSectors;
-
-    }
-
     function dropper(block, sector) {
 
         var newLevel = $("<div />", {
@@ -406,8 +222,10 @@ $(document).ready(function () {
         block.selectBlock.hide();
         block.parentBlock.html('');
 
+
     }
 
+    //  GENERATE NEW BLOCKS
     function generateNewBlock(block) {
 
         var newElement = $("<div />", {
@@ -424,5 +242,134 @@ $(document).ready(function () {
 
         $(".dinamicBlock").html(newElement);
     }
+    function getNewRandomElement() {
 
+        // Generate random number
+        var Random = Math.floor((Math.random() * Object.keys(Core).length) + 1);
+
+        // Keys of Core
+        var keysOfCore = Object.keys(Core);
+
+        // Sort all of Core's values
+        if(Core[keysOfCore[Random-1]].hasOwnProperty('link')){
+            return Core[keysOfCore[Random-1]];
+        } else {
+            return getNewRandomElement();
+        }
+
+    }
+    function createNewElement(Obj) {
+
+        var newElement = $("<div />", {
+            class: "block",
+            element: Obj.name,
+            css: {
+                "background-color":Obj.color,
+                "background-image":"url('img/"+Obj.name+".png')",
+                "background-size":"80%",
+                "background-repeat":"no-repeat",
+                "background-position":"center",
+            }
+        });
+
+        $(".dinamicBlock").html(newElement);
+
+    }
+
+    //  AWARDS & TIMER
+
+    //  TIMER FUNCs
+    $(".start").click(function () {
+        $(".time").show('');
+        $(".points").show('');
+        $(".points").html('0');
+        $(".battleplace .sector").html('');
+
+        $(".start").hide();
+
+        var time = 60;
+
+        setInterval(function() {
+
+            $(".time").html(time);
+            console.log(time);
+
+            if(time == 0) {
+                var r = confirm("Your score: "+Results+"! Do you want to play again?");
+                if (r == true) {
+                    $(".time").html('');
+                    $(".points").html('');
+                    $(".start").hide();
+                    $(".battleplace .sector").html('');
+                    time = 60;
+                    Results = 0;
+                    clearAwards();
+                } else {
+                    location.reload();
+                }
+            } else {
+                time--;
+            }
+        }, 1000);
+
+    });
+    $(".time").hide('');
+    $(".points").hide('');
+
+    //  AWARDS FUNCs
+    function plusPoints() {
+        Results += 100;
+    }
+    function minusPoints() {
+        Results -= 100;
+    }
+    function updateResults(){
+        $(".points").html(Results);
+    }
+    function checkAwards(){
+
+        $(".awards").html('');
+
+        for(var name in AwardsMemory) {
+            // propertyName is what you want
+            // you can get the value like this: myObject[propertyName]
+
+            if((AwardsMemory[name] < 3)&&(AwardsMemory[name] > 0)) {
+                $(".awards").append("<div class='award'><h4>"+name+" = "+AwardsMemory[name]+"!</h4></div>");
+            }
+
+            if(AwardsMemory[name] == 3) {
+                $(".awards").append("<div class='award'><h4>"+name+" > 3! WOOOOW!</h4></div>");
+            }
+
+            if((AwardsMemory[name] < 6)&&(AwardsMemory[name] > 3)) {
+                $(".awards").append("<div class='award'><h4>"+name+" = "+AwardsMemory[name]+"!</h4></div>");
+            }
+
+            if(AwardsMemory[name] == 6) {
+                $(".awards").append("<div class='award'><h4>"+name+" > 6! AWESOME!</h4></div>");
+            }
+
+            if((AwardsMemory[name] < 9)&&(AwardsMemory[name] > 6)) {
+                $(".awards").append("<div class='award'><h4>"+name+" = "+AwardsMemory[name]+"!</h4></div>");
+            }
+
+        }
+    }
+    function clearAwards(){
+
+        $(".awards").html('');
+
+        for(var name in AwardsMemory) {
+            // propertyName is what you want
+            // you can get the value like this: myObject[propertyName]
+
+            AwardsMemory[name] = 0;
+
+        }
+    }
+
+
+    //  DEFAULT SETTINGS
+    createNewElement(getNewRandomElement());
 });
